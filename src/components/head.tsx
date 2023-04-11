a501309623135d9ec08977cc521bca866041cebd
Matthew Bierman <matthew.bierman2@gmail.com>
2023-04-11 02:25:20 -0400

    Favicon

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -37,6 +37,8 @@ export const Head: FC<{ description?: string; title?: string }> = ({
       <meta name="description" content={description || ""} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="theme-color" content="#35363a" />
       <GoogleAnalytics />
     </NextHead>
   );
